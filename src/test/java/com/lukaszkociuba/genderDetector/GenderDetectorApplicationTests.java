dba91607784cb32a6db7b5947f9a1d2ab4eb397b
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-21 17:48:42 +0100

    Set up project using Spring initializr

---
+++ src/test/java/com/lukaszkociuba/genderDetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszkociuba.genderDetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
