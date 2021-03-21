eb97271dd435fe980dab25a486cfdd393d8d2830
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-21 19:14:13 +0100

    Add NameToToken conversion functionality
     - add lambda with NameToToken interface to convert inputted name to
     tokens.
     - add Array of String's to reuse it in methods.
     - add NameToToken conversions to methods "getGenderByFirstTokenOnly"
     and "getGenderByAllTokens".

--- src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectorImpl.java
+++ src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectorImpl.java
@@ -1,13 +1,19 @@
 package com.lukaszkociuba.genderDetector.domain;
 
 public class GenderDetectorImpl implements GenderDetector {
+
+    private NameToTokenConverter nameToTokenConverter = (name -> name.split("\\s"));
+    private String[] tokenedName;
+
     @Override
     public String getGenderByFirstTokenOnly(String name) {
+        tokenedName = nameToTokenConverter.splitNameIntoTokens(name);
         return null;
     }
 
     @Override
     public String getGenderByAllTokens(String name) {
+        tokenedName = nameToTokenConverter.splitNameIntoTokens(name);
         return null;
     }
 }
