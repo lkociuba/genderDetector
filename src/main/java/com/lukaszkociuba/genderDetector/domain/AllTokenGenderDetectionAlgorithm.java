2d4bc141644c835d6b638b7fbbc5b04231bd39ca
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-22 22:53:51 +0100

    Add "AllTokenGenderDetectionAlgorithm" & test
     - factory pattern.

---
+++ src/main/java/com/lukaszkociuba/genderDetector/domain/AllTokenGenderDetectionAlgorithm.java
@@ -0,0 +1,39 @@
+package com.lukaszkociuba.genderDetector.domain;
+
+import java.util.List;
+
+public class AllTokenGenderDetectionAlgorithm implements GenderDetectionAlgorithm {
+
+    private int countEqualTokens(String[] tokenedName, List<String> nameTokens) {
+        int equalTokens = 0;
+        for (var nameToken : tokenedName) {
+            for (var femaleToken : nameTokens) {
+                if (nameToken.equals(femaleToken)) {
+                    equalTokens += 1;
+                }
+            }
+        }
+        return equalTokens;
+    }
+
+    @Override
+    public String detectGender(String name, List<String> femaleTokens, List<String> maleTokens) {
+        if (name == null || femaleTokens == null || maleTokens == null) {
+            throw new NullPointerException("Null value given!");
+        }
+
+        NameToTokenConverter nameToTokenConverter = (name1 -> name1.split("\\s"));
+        var tokenedName = nameToTokenConverter.splitNameIntoTokens(name);
+
+        int countFemaleTokens = countEqualTokens(tokenedName, femaleTokens);
+        int countMaleTokens = countEqualTokens(tokenedName, maleTokens);
+
+        if (countFemaleTokens != 0 && countFemaleTokens > countMaleTokens) {
+            return "FEMALE";
+        } else if (countMaleTokens != 0 && countMaleTokens > countFemaleTokens) {
+            return "MALE";
+        } else {
+            return "INCONCLUSIVE";
+        }
+    }
+}
---
+++ src/test/java/com/lukaszkociuba/genderDetector/domain/AllTokenGenderDetectionAlgorithmTest.java
@@ -0,0 +1,61 @@
+package com.lukaszkociuba.genderDetector.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllTokenGenderDetectionAlgorithmTest {
+
+    private AllTokenGenderDetectionAlgorithm allTokenGenderDetectionAlgorithm = new AllTokenGenderDetectionAlgorithm();
+
+    private final String name1 = "Jan Maria Rokita";
+    private final List<String> femaleTokens = new ArrayList<>(Arrays.asList("Maria", "Anna", "Gertruda"));
+    private final List<String> maleTokens = new ArrayList<>(Arrays.asList("Jan", "Andrzej", "Olaf"));
+
+    @Test
+    void detectGender_name1_resultInconclusive() {
+        var result = allTokenGenderDetectionAlgorithm.detectGender(name1, femaleTokens, maleTokens);
+
+        assertThat(result, is("INCONCLUSIVE"));
+    }
+
+    @Test
+    void detectGender_name2_resultFemale() {
+        var name2 = "Anna Zgidniew Gertruda";
+        var result = allTokenGenderDetectionAlgorithm.detectGender(name2, femaleTokens, maleTokens);
+
+        assertThat(result, is("FEMALE"));
+    }
+
+    @Test
+    void detectGender_name3_resultMale() {
+        var name3 = "Anna Olaf Jan";
+        var result = allTokenGenderDetectionAlgorithm.detectGender(name3, femaleTokens, maleTokens);
+
+        assertThat(result, is("MALE"));
+    }
+
+    @Test
+    void detectGender_nullName_returnNullPointerException() {
+        assertThrows(NullPointerException.class, () ->
+                allTokenGenderDetectionAlgorithm.detectGender(null, femaleTokens, maleTokens));
+    }
+
+    @Test
+    void detectGender_nullFemaleTokens_returnNullPointerException() {
+        assertThrows(NullPointerException.class, () ->
+                allTokenGenderDetectionAlgorithm.detectGender(name1, null, maleTokens));
+    }
+
+    @Test
+    void detectGender_nullMaleTokens_returnNullPointerException() {
+        assertThrows(NullPointerException.class, () ->
+                allTokenGenderDetectionAlgorithm.detectGender(name1, femaleTokens, null));
+    }
+}
