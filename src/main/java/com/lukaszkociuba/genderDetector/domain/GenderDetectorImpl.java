7bcb508c1eb7d9afd2a5cd25e4dea6fb854d7e73
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-22 11:44:59 +0100

    Add functionality to "getGenderByAllTokens"
     - add "AdvancedAlgorithm" functionality.
     - run unit tests.

--- src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectorImpl.java
+++ src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectorImpl.java
@@ -2,6 +2,7 @@ package com.lukaszkociuba.genderDetector.domain;
 
 public class GenderDetectorImpl implements GenderDetector {
     private SimpleAlgorithm simpleAlgorithm = new SimpleAlgorithm();
+    private AdvancedAlgorithm advancedAlgorithm = new AdvancedAlgorithm();
 
     private NameToTokenConverter nameToTokenConverter = (name -> name.split("\\s"));
     private String[] tokenedName;
@@ -13,15 +14,18 @@ public class GenderDetectorImpl implements GenderDetector {
         }
 
         tokenedName = nameToTokenConverter.splitNameIntoTokens(name);
-        var result = simpleAlgorithm.predictGenderByFirstTokenOnly(tokenedName);
 
-        return result;
+        return simpleAlgorithm.predictGenderByFirstTokenOnly(tokenedName);
     }
 
     @Override
     public String getGenderByAllTokens(String name) {
+        if (name == null) {
+            throw new NullPointerException("Null value given!");
+        }
+
         tokenedName = nameToTokenConverter.splitNameIntoTokens(name);
 
-        return null;
+        return advancedAlgorithm.predictGenderByAllTokens(tokenedName);
     }
 }
