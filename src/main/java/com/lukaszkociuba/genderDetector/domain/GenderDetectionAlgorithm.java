5504836d4083f1b51d44ec129f2ef387484a6cb8
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-22 20:51:26 +0100

    Add "GenderDetectionAlgorithm" - factory pattern

---
+++ src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectionAlgorithm.java
@@ -0,0 +1,7 @@
+package com.lukaszkociuba.genderDetector.domain;
+
+import java.util.List;
+
+public interface GenderDetectionAlgorithm {
+    String detectGender (String name, List<String> femaleTokens, List<String> maleTokens);
+}
