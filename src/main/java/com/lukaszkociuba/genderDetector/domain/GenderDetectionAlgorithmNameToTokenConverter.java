28f7b251bd5af842a0a0bbdd9eb526ee8d14a289
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-24 19:59:30 +0100

    Remove unnecessary interface

--- src/main/java/com/lukaszkociuba/genderDetector/domain/GenderDetectionAlgorithmNameToTokenConverter.java
+++
@@ -1,7 +0,0 @@
-package com.lukaszkociuba.genderDetector.domain;
-
-@FunctionalInterface
-public interface GenderDetectionAlgorithmNameToTokenConverter {
-
-    String[] splitNameIntoTokens(String name);
-}
