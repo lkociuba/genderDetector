718c464723647d19f75ffb8397f34883e3b5d7cc
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-24 22:59:40 +0100

    Refactor "GenderDetectionAlgorithmServiceV1Test"
     - refactor test names.
     - refactor test scenarios.

--- src/test/java/com/lukaszkociuba/genderDetector/domain/GenderDetectionAlgorithmServiceV1Test.java
+++ src/test/java/com/lukaszkociuba/genderDetector/domain/GenderDetectionAlgorithmServiceV1Test.java
@@ -7,68 +7,55 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenderDetectionAlgorithmServiceV1Test {
-
     private GenderDetectionAlgorithmServiceV1 genderDetectionAlgorithmService = new GenderDetectionAlgorithmServiceV1();
 
-    private final String firstToken = "firstToken";
-    private final String allTokens = "allTokens";
-
-    private final String name1 = "Jan Maria Rokita";
-    private final String name2 = "Anna Zgidniew Gertruda";
+    private final String name1 = "Jan Maria Gertruda";
 
     @Test
-    void detectGender_name1FirstToken_resultMale() throws Exception {
-        var result = genderDetectionAlgorithmService.detectGender(name1, firstToken);
+    void shouldReturnMaleGenderForFirstTokenAlgorithmType() throws Exception {
+        var algorithmType = "firstToken";
+
+        var result = genderDetectionAlgorithmService.detectGender(name1, algorithmType);
 
         assertThat(result, is("MALE"));
     }
 
     @Test
-    void detectGender_name2FirstToken_resultMale() throws Exception {
-        var result = genderDetectionAlgorithmService.detectGender(name2, firstToken);
+    void shouldReturnFemaleGenderForAllTokensAlgorithmType() throws Exception {
+        var algorithmType = "allTokens";
+
+        var result = genderDetectionAlgorithmService.detectGender(name1, algorithmType);
 
         assertThat(result, is("FEMALE"));
     }
 
     @Test
-    void detectGender_name3FirstToken_resultInconclusive() throws Exception {
-        var name3 = "Krzysztof Bogusław";
-        var result = genderDetectionAlgorithmService.detectGender(name3, firstToken);
+    void shouldReturnExceptionForWrongAlgorithmType() {
+        var algorithmType = "wrongType";
 
-        assertThat(result, is("INCONCLUSIVE"));
+        assertThrows(Exception.class, () ->
+                genderDetectionAlgorithmService.detectGender(name1, algorithmType));
     }
 
     @Test
-    void detectGender_nullNameFirstToken_resultNullPointerException() {
-        assertThrows(NullPointerException.class, () ->
-                genderDetectionAlgorithmService.detectGender(null, firstToken));
+    void shouldReturnExceptionForEmptyAlgorithmType() {
+        var algorithmType = "";
+
+        assertThrows(Exception.class, () ->
+                genderDetectionAlgorithmService.detectGender(name1, algorithmType));
     }
 
     @Test
-    void detectGender_name1NullAlgorithmType_resultException() {
+    void shouldReturnExceptionForSpaceAlgorithmType() {
+        var algorithmType = "";
+
+        assertThrows(Exception.class, () ->
+                genderDetectionAlgorithmService.detectGender(name1, algorithmType));
+    }
+
+    @Test
+    void shouldReturnExceptionForNullAlgorithmType() {
         assertThrows(Exception.class, () ->
                 genderDetectionAlgorithmService.detectGender(name1, null));
-    }
-
-    @Test
-    void detectGender_name1AllTokens_resultInconclusive() throws Exception {
-        var result = genderDetectionAlgorithmService.detectGender(name1, allTokens);
-
-        assertThat(result, is("INCONCLUSIVE"));
-    }
-
-    @Test
-    void detectGender_name2AllTokens_resultFemale() throws Exception {
-        var result = genderDetectionAlgorithmService.detectGender(name2, allTokens);
-
-        assertThat(result, is("FEMALE"));
-    }
-
-    @Test
-    void detectGender_name4AllTokens_resulMale() throws Exception {
-        var name4 = "Anna Olaf Jan";
-        var result = genderDetectionAlgorithmService.detectGender(name4, allTokens);
-
-        assertThat(result, is("MALE"));
     }
 }
