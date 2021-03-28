babf210cd6ede1e565750ddaf1f2b5555ec21643
Łukasz Kociuba <kociolek00@gmail.com>
2021-03-28 23:37:19 +0200

    Refactor "FirstTokenGenderDetectionAlgorithm"

--- src/main/java/com/lukaszkociuba/genderDetector/domain/model/FirstTokenGenderDetectionAlgorithm.java
+++ src/main/java/com/lukaszkociuba/genderDetector/domain/model/FirstTokenGenderDetectionAlgorithm.java
@@ -1,32 +1,28 @@
 package com.lukaszkociuba.genderDetector.domain.model;
 
+import com.lukaszkociuba.genderDetector.domain.port.FileTokenSource;
 import com.lukaszkociuba.genderDetector.domain.port.GenderDetectionAlgorithm;
 
 import java.util.List;
 
 public class FirstTokenGenderDetectionAlgorithm implements GenderDetectionAlgorithm {
 
-    private NameToTokenConverter nameToTokenConverter =
-            new NameToTokenConverter();
-
     @Override
-    public GenderDetectionAlgorithmResult detectGender(String name, List<String> femaleTokens, List<String> maleTokens) {
-        var tokenedName = nameToTokenConverter
-                .splitToTokens(name, femaleTokens, maleTokens);
-        var firstTokenOfName = tokenedName[0];
+    public GenderDetectionAlgorithmResult detectGender(String name, FileTokenSource femaleTokensSource, FileTokenSource maleTokensSource) {
 
-        for (var femaleToken : femaleTokens) {
-            if (femaleToken.equals(firstTokenOfName)) {
-                return GenderDetectionAlgorithmResult.FEMALE;
-            }
+        boolean isFemale = femaleTokensSource.stream()
+                .anyMatch(name::startsWith);
+        if (isFemale) {
+            return GenderDetectionAlgorithmResult.FEMALE;
         }
 
-        for (var maleToken : maleTokens) {
-            if (maleToken.equals(firstTokenOfName)) {
-                return GenderDetectionAlgorithmResult.MALE;
-            }
-        }
+        boolean isMale = maleTokensSource.stream()
+                .anyMatch(name::startsWith);
 
-        return GenderDetectionAlgorithmResult.INCONCLUSIVE;
+        if (isMale) {
+            return GenderDetectionAlgorithmResult.MALE;
+        } else {
+            return GenderDetectionAlgorithmResult.INCONCLUSIVE;
+        }
     }
 }
